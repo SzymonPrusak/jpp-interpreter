d1e13b0b19331161279d0ee5974b4cc47f72def9
SzymonPrusak <szymon.prusak.1@gmail.com>
2022-05-12 13:20:59 +0200

    fun call 2

--- Interpreter.hs
+++ Interpreter.hs
@@ -4,7 +4,7 @@ import qualified Data.Map as M
 import Data.Maybe (fromMaybe)
 import Control.Monad.Except (ExceptT, MonadError (throwError), runExceptT)
 import Control.Monad.Identity (Identity (runIdentity))
-import Control.Monad.Reader (ReaderT (runReaderT), MonadReader (ask))
+import Control.Monad.Reader (ReaderT (runReaderT), MonadReader (ask, local))
 import Control.Monad.State (StateT, MonadState (get, put), evalStateT)
 
 import Common
@@ -72,8 +72,6 @@ data IPGlobalEnv = IPGEnv {
     nextAddress :: Int
     }
 
-type IPGEnvMod = IPGlobalEnv -> IPGlobalEnv
-
 newGlobalEnv :: [FunDef] -> IPGlobalEnv
 newGlobalEnv fds = IPGEnv { callMap = buildCallMap fds, stack = M.empty, nextAddress = 0 }
 
@@ -83,11 +81,26 @@ type VarMap = M.Map VarName VarAddress
 
 data IPLocalEnv = IPLEnv {
     blockName :: BlockQName,
-    varMap :: VarMap
+    varMap :: VarMap,
+    nextSubBlockIndex :: Int
     }
 
+type IPLEnvMod = IPLocalEnv -> IPLocalEnv
+
 newLocalEnv :: IPLocalEnv
-newLocalEnv = IPLEnv { blockName = [], varMap = M.empty }
+newLocalEnv = IPLEnv { blockName = [], varMap = M.empty, nextSubBlockIndex = 0 }
+
+setBlock :: BlockQName -> IPLEnvMod
+setBlock ns env = env { blockName = ns, nextSubBlockIndex = 0 }
+
+enterBlock :: Ident -> IPLEnvMod
+enterBlock n env = setBlock (n:blockName env) env
+
+bindVar :: VarName -> VarAddress -> IPLEnvMod
+bindVar vn addr env = env { varMap = M.insert vn addr $ varMap env }
+
+nextSubBlock :: IPLEnvMod
+nextSubBlock env = env { nextSubBlockIndex = nextSubBlockIndex env + 1 }
 
 
 data RuntimeException =
@@ -106,35 +119,46 @@ runInterpreter fds = (runIdentity . runExceptT . (`runReaderT` newLocalEnv) . (`
     runInterpreter :: Interpreter ()
     runInterpreter = do
         mmain <- resolveCall (Ident "main")
-        main <- maybe (throwError EntryPointNotFound) return mmain
-        callFunDef main []
+        (main, ns) <- maybe (throwError EntryPointNotFound) return mmain
+        callFunDef main ns []
         return ()
 
 
-resolveCall :: FunName -> Interpreter (Maybe FunDef)
+resolveCall :: FunName -> Interpreter (Maybe (FunDef, BlockQName))
 resolveCall fn = do
     genv <- get
     lenv <- ask
     return $ resolveCall fn (blockName lenv) (callMap genv)
     where
-        resolveCall :: FunName -> BlockQName -> CallMap -> Maybe FunDef
+        resolveCall :: FunName -> BlockQName -> CallMap -> Maybe (FunDef, BlockQName)
         resolveCall fn [] m = do
             fm <- M.lookup [] m
-            M.lookup fn fm
-        resolveCall fn (i:is) m = case M.lookup (i:is) m of
+            lookupFun fn fm []
+        resolveCall fn ns@(i:is) m = case M.lookup ns m of
             Nothing -> resolveCall fn is m
-            Just fm -> M.lookup fn fm
+            Just fm -> lookupFun fn fm (i:is)
+        lookupFun :: FunName -> FunMap -> BlockQName -> Maybe (FunDef, BlockQName)
+        lookupFun fn fm ns = (\d -> (d, ns)) <$> M.lookup fn fm
 
 callFunction :: FunName -> [VarValue] -> Interpreter (Maybe VarValue)
 callFunction fn args = do
     mfun <- resolveCall fn
     case mfun of
         Nothing -> fatalError $ "function " ++ printTree fn ++ " not found - should be handled by TC"
-        Just fd -> callFunDef fd args
+        Just (fd, ns) -> callFunDef fd ns args
 
-callFunDef :: FunDef -> [VarValue] -> Interpreter (Maybe VarValue)
-callFunDef fd args = do
-    return Nothing
+callFunDef :: FunDef -> BlockQName -> [VarValue] -> Interpreter (Maybe VarValue)
+callFunDef (FunDefin _ _ fn params sb) ns args = do
+    lenv1 <- setArguments params args
+    let lenv2 = setBlock (fn:ns) lenv2
+    local (const lenv2) $ execBlock sb
+    where
+        setArguments :: [FunParam] -> [VarValue] -> Interpreter IPLocalEnv
+        setArguments ((FunPar _ _ vn):ps) (a:as) = do
+            addr <- allocVar
+            setVar addr a
+            local (bindVar vn addr) $ setArguments ps as
+        setArguments _ _ = ask
 
 
 allocVar :: Interpreter VarAddress
@@ -143,3 +167,13 @@ allocVar = do
     let addr = nextAddress genv
     put genv { nextAddress = addr + 1 }
     return addr
+
+setVar :: VarAddress -> VarValue -> Interpreter ()
+setVar addr val = do
+    genv <- get
+    let nstack = M.insert addr val $ stack genv
+    put genv { stack = nstack }
+
+
+execBlock :: StmtBlock -> Interpreter (Maybe VarValue)
+execBlock = undefined
